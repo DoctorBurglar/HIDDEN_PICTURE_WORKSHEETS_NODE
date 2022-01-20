943f00cd48b0fb1c4cd079fa4228d0730b79f843
Steve Lorentzen <Stevelorentzen@gmail.com>
2022-01-20 11:25:29 -0800

    fixed deleteStudent from classroom functionality

--- middleware/is-teacher.js
+++ middleware/is-teacher.js
@@ -1,5 +1,4 @@
 module.exports = (req, res, next) => {
-  console.log(req);
   const roles = req.user["https://hiddenpicturetest.com/roles"];
   console.log(roles);
   if (!roles.includes("teacher")) {
